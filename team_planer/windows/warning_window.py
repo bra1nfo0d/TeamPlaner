511db6c464bcdce00baaf0a3a292e6189352b685
Sergej Gurov <sergej.gurov@outlook.de>
2025-10-08 17:16:53 +0200

    refactor(ui): rename error_window to popup_window and extend class functionality

---
+++ team_planer/windows/warning_window.py
@@ -0,0 +1,41 @@
+from PySide6.QtWidgets import QMessageBox
+from PySide6.QtCore import Qt
+from team_planer.core.config_manager import ConfigManager
+
+
+class PopupWindow(QMessageBox):
+	"""Popup window showing error message from config."""
+
+	def __init__(self, popup_type: str, text_code: str, parent: object | None = None):
+		"""
+		Args:
+			error_code (str): Error code used to look up massage text.
+			parent (object): Parent widget.
+		"""
+		super().__init__(parent)
+		self.config_manager = ConfigManager()
+		self.popup_type = popup_type
+		self.text_code = text_code
+
+		self._setup_window()
+
+	def _setup_window(self) -> None:
+		"""Configurate window title, text, and icon from config."""
+		config = self.config_manager.load_config()
+		header_text_list = config[self.text_code]
+		header = header_text_list[0]
+		text = header_text_list[1]
+
+		self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint)
+		self.setIcon(QMessageBox.Warning)
+		self.setWindowTitle(header)
+		self.setText(text)
+		if self.popup_type == "error":
+			self.setStandardButtons(QMessageBox.Ok)
+		elif self.popup_type == "accept":
+			self.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+		
+
+
+if __name__ == "__main__":
+	pass
