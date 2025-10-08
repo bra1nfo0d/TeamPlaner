1be82c21e0b17a18c0d0ff63afd6d2dece89a73f
Sergej Gurov <sergej.gurov@outlook.de>
2025-10-08 13:37:06 +0200

    refactor(ui): update imports after merging clickable_frame and clickable_label

--- team_planer/ui_elements/day_view.py
+++ team_planer/ui_elements/day_view.py
@@ -3,7 +3,7 @@ from PySide6.QtCore import Qt
 from PySide6.QtGui import QFont
 from team_planer.core.date_manager import DateManager
 from team_planer.core.config_manager import ConfigManager
-from team_planer.ui_elements.clickable_label import ClickableLabel
+from team_planer.ui_elements.clickable_widgets import ClickableLabel
 from team_planer.windows.input_window import InputWindow
 
 class DayView(QWidget):
